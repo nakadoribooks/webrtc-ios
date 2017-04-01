697532969d8866f4edf1d66b4a556584fa95f47b
yu kawase <kawase@aloe-project.com>
2017-04-01 13:40:09 +0900

    Initial Commit

---
+++ webrtcExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webrtcExample
+//
+//  Created by kawase yu on 2017/04/01.
+//  Copyright © 2017年 NakadoriBooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
